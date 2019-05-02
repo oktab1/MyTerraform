e3f6cd7b46319ca8e282d0faab0f776bb4b33eb6
Tolulope Okuboyejo <tokuboyejo@dxc.com>
2019-05-02 15:18:20 -0500

    Updating changes

--- docker.tf
+++ docker.tf
@@ -22,7 +22,7 @@ resource "aws_vpc" "vpc" {
   cidr_block = "172.24.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    Name = "tmp.refactor.zone"
+    Name = "awssd2.webdatalinks.zone"
   }
 }
 
@@ -30,7 +30,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.vpc.id}"
   tags = {
-    Name = "tmp.refactor.zone"
+    Name = "awssd2.webdatalinks.zone"
   }
 }
 
@@ -41,7 +41,7 @@ resource "aws_subnet" "subnet" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${element(var.ranges, count.index)}"
   tags = {
-    Name = "${element(var.zones, count.index)}.tmp.refactor.zone"
+    Name = "${element(var.zones, count.index)}.awssd2.webdatalinks.zone"
   }
 }
 
@@ -99,7 +99,7 @@ resource "aws_default_security_group" "sg" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "tmp.refactor.zone"
+    Name = "awssd2.webdatalinks.zone"
   }
 }
 
@@ -121,54 +121,46 @@ data "aws_ami" "ubuntu" {
 
 # Create the ECS role so the container can connect to the ECS service
 resource "aws_iam_role" "iam-role" {
-  name = "tmp-refactor-zone"
+  name = "awssd2-webdatalinks-zone"
 
   assume_role_policy = "${file("files/ecs-iam-role.json")}"
 }
 
 resource "aws_iam_role_policy" "iam-role-policy" {
-  name = "tmp-refactor-zone"
+  name = "awssd2-webdatalinks-zone"
   role = "${aws_iam_role.iam-role.id}"
 
   policy = "${file("files/ecs-iam-role-policy.json")}"
 }
 
 resource "aws_iam_instance_profile" "iam_profile" {
-  name  = "tmp-refactor-zone"
+  name  = "awssd2-webdatalinks-zone"
   role = "${aws_iam_role.iam-role.name}"
 }
 
 # Launch configurations describe the instance to be launched
 resource "aws_launch_configuration" "default" {
-  name_prefix                 = "tmp.refactor.zone-"
+  name_prefix                 = "awssd2.webdatalinks.zone-"
   image_id                    = "${data.aws_ami.ubuntu.id}"
   instance_type               = "m4.large"
   user_data                   = <<USERDATA
 #!/bin/bash
-
 set -e
-
 useradd -m -s /bin/bash janoszen
 mkdir /home/janoszen/.ssh
 echo 'ssh-rsa AAAA....' >/home/janoszen/.ssh/authorized_keys
 gpasswd -a janoszen sudo
-
 echo 'Defaults  env_reset
 Defaults        mail_badpass
 Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
 root    ALL=(ALL:ALL) ALL
 %admin ALL=(ALL) NOPASSWD: ALL
 %sudo   ALL=(ALL:ALL) NOPASSWD: ALL' >/etc/sudoers
-
 export DEBIAN_FRONTEND=noninteractive
-
 mkdir -p /etc/ecs
-
 echo ECS_CLUSTER=${aws_ecs_cluster.ecs.name} > /etc/ecs/ecs.config
-
 apt-get -q -y update && apt-get -q -y upgrade && apt-get -q -y dist-upgrade
 apt-get -q -y install docker.io
-
 mkdir -p /var/log/ecs /etc/ecs /var/lib/ecs/data
 sysctl -w net.ipv4.conf.all.route_localnet=1
 iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679
@@ -186,10 +178,8 @@ docker run --name ecs-agent \
     --env=ECS_ENABLE_TASK_IAM_ROLE=true \
     --env=ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true \
     amazon/amazon-ecs-agent:latest
-
 userdel -f -r ubuntu
 rm -rf /root/.ssh/authorized_keys
-
 USERDATA
   associate_public_ip_address = true
   # Associate the IAM profile tor ECS
@@ -206,7 +196,7 @@ USERDATA
 # Create the autoscaling group for the host machines
 resource "aws_autoscaling_group" "default" {
   availability_zones        = ["${var.zones}"]
-  name                      = "tmp.refactor.zone"
+  name                      = "awssd2.webdatalinks.zone"
   max_size                  = 3
   min_size                  = 1
   health_check_grace_period = 300
@@ -218,11 +208,11 @@ resource "aws_autoscaling_group" "default" {
 }
 #Add the ECS stuff
 resource "aws_ecs_cluster" "ecs" {
-  name = "tmp-refactor-zone"
+  name = "awssd2-webdatalinks-zone"
 }
 
 resource "aws_ecr_repository" "www" {
-  name = "tmp-refactor-zone"
+  name = "awssd2-webdatalinks-zone"
   provisioner "local-exec" {
     command = "$(aws ecr get-login --region us-east-2) && docker build -t ${aws_ecr_repository.www.name} . && docker tag ${aws_ecr_repository.www.name}:latest ${aws_ecr_repository.www.repository_url}:latest && docker push ${aws_ecr_repository.www.repository_url}:latest"
   }
@@ -261,7 +251,7 @@ DEFINITION
 
 # Create load balancer
 resource "aws_elb" "elb" {
-  name               = "tmp-refactor-zone"
+  name               = "awssd2-webdatalinks-zone"
   security_groups    = ["${aws_default_security_group.sg.id}"]
   subnets            = ["${aws_subnet.subnet.*.id}"]
 
@@ -297,8 +287,8 @@ resource "aws_ecs_service" "www" {
 
 # Route 53
 data "aws_route53_zone" "zone" {
-  name = "tmp.refactor.zone."
-  private_zone=true
+  name = "awssd2.webdatalinks.zone."
+  private_zone = true
 }
 
 resource "aws_route53_record" "www" {
